8de124c60da5e84e6677d5aca561d3afbad94cfb
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-07-16 16:11:04 +0700

    init comit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Welcome from './Welcome'
+import reportWebVitals from './reportWebVitals';
+
+import Comment from './Comment'
+
+const obj = {
+  author : {
+    avatarUrl : "https://images-ext-1.discordapp.net/external/PIPWYtqms7laKOw1ect6Ams3U5pBlBWtQtYfQGUCQTQ/%3Fq%3Dtbn%3AANd9GcRGNens_IGL9MqtOmRCvPhC2fbq4Mkra_OQNjT_L88N2TyviO0g/https/encrypted-tbn2.gstatic.com/images?width=348&height=521",
+    name : "Paulus B. Siahaan"
+  }, 
+  text: "Learn React Js",
+  date: "15 Juli 2021"
+} 
+ReactDOM.render(
+  <React.StrictMode>
+    <Welcome name="Sara" />
+    <App {...obj} />
+    <Comment {...obj} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
